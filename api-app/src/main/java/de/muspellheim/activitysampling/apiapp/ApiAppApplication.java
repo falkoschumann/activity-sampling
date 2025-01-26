29d2cd2faf49a2a4eca79e256e38ae9dd3e31864
Falko Schumann <falko.schumann@muspellheim.de>
2025-01-26 18:19:31 +0100

    feat: create API app stub

---
+++ api-app/src/main/java/de/muspellheim/activitysampling/apiapp/ApiAppApplication.java
@@ -0,0 +1,12 @@
+package de.muspellheim.activitysampling.apiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiAppApplication.class, args);
+  }
+}
---
+++ api-app/src/test/java/de/muspellheim/activitysampling/apiapp/ApiAppApplicationTests.java
@@ -0,0 +1,11 @@
+package de.muspellheim.activitysampling.apiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
