0969f11775f6905c8b4bf8285d65e65238cb0f53
Falko Schumann <falko.schumann@muspellheim.de>
2025-01-28 16:21:57 +0100

    refactor: rename package and class

--- api-app/src/main/java/de/muspellheim/activitysampling/api/ApiApplication.java
+++ api-app/src/main/java/de/muspellheim/activitysampling/api/ApiApplication.java
@@ -1,14 +1,14 @@
 /* Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license. */
 
-package de.muspellheim.activitysampling.apiapp;
+package de.muspellheim.activitysampling.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiAppApplication {
+public class ApiApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiAppApplication.class, args);
+    SpringApplication.run(ApiApplication.class, args);
   }
 }
--- api-app/src/test/java/de/muspellheim/activitysampling/api/ApiApplicationTests.java
+++ api-app/src/test/java/de/muspellheim/activitysampling/api/ApiApplicationTests.java
@@ -1,12 +1,12 @@
 /* Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license. */
 
-package de.muspellheim.activitysampling.apiapp;
+package de.muspellheim.activitysampling.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiAppApplicationTests {
+class ApiApplicationTests {
 
   @Test
   void contextLoads() {}
