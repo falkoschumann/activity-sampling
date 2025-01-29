240e12c79029b80822c38d51e4ddbe0d2feafeaa
Falko Schumann <falko.schumann@muspellheim.de>
2025-01-29 19:39:12 +0100

    feat: fetch data in client

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,15 +1,44 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Countdown from "@/components/countdown";
 import CurrentActivity from "@/components/current_activity";
 import RecentActivities from "@/components/recent_activities";
 import TimeSummary from "@/components/time_summary";
 import { ActivitiesApi } from "@/lib/activities_api";
+import { RecentActivitiesQueryResult } from "@/lib/domain";
+import { Duration } from "@/lib/duration";
 
 const activitiesApi = new ActivitiesApi();
 
-export default async function Home() {
-  const recentActivities = await activitiesApi.queryRecentActivities();
+export default function Home() {
+  const [recentActivities, setRecentActivities] = useState<RecentActivitiesQueryResult>({
+    workingDays: [],
+    timeSummary: {
+      hoursToday: Duration.ZERO,
+      hoursYesterday: Duration.ZERO,
+      hoursThisWeek: Duration.ZERO,
+      hoursThisMonth: Duration.ZERO,
+    },
+  });
+
+  useEffect(() => {
+    console.log("query recent activities");
+    let ignore = false;
+    (async () => {
+      const result = await activitiesApi.queryRecentActivities();
+      if (!ignore) {
+        setRecentActivities(result);
+      }
+    })();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <>
       <aside className="container-fluid fixed-top py-2 bg-body">
