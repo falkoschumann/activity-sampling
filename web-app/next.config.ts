3b0591e7ba79b6969a3f8d108857d694e494049b
Falko Schumann <falko.schumann@muspellheim.de>
2025-01-30 17:47:25 +0100

    build: clean out folder

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,29 +1,31 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
 import type { NextConfig } from "next";
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+import { PHASE_DEVELOPMENT_SERVER, PHASE_EXPORT } from "next/constants";
 
 function nextConfig(phase: string): NextConfig {
-  const defaultConfig: NextConfig = {};
+  let config: NextConfig = {};
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...defaultConfig,
-      rewrites: async () => {
-        return [
-          {
-            source: "/api/:path*",
-            destination: "http://localhost:8080/api/:path*",
-          },
-        ];
-      },
+    config = {
+      ...config,
+      rewrites: async () => [
+        {
+          source: "/api/:path*",
+          destination: "http://localhost:8080/api/:path*",
+        },
+      ],
     };
   }
 
-  return {
-    ...defaultConfig,
-    output: "export",
-  };
+  if (phase === PHASE_EXPORT) {
+    config = {
+      ...config,
+      output: "export",
+    };
+  }
+
+  return config;
 }
 
 export default nextConfig;
