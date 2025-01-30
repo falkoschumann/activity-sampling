878fe5f37b30051c25003e9940dedd993b3e58ed
Falko Schumann <falko.schumann@muspellheim.de>
2025-01-30 14:12:16 +0100

    refactor: move test

--- api-app/src/test/java/de/muspellheim/activitysampling/api/e2e/ApiApplicationTests.java
+++ api-app/src/test/java/de/muspellheim/activitysampling/api/e2e/ApiApplicationTests.java
@@ -1,6 +1,6 @@
 /* Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license. */
 
-package de.muspellheim.activitysampling.api;
+package de.muspellheim.activitysampling.api.e2e;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
