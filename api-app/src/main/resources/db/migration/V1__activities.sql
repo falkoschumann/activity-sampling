b17855d0a186e3fa6c8ccc30757c18d8f4cd3840
Falko Schumann <falko.schumann@muspellheim.de>
2025-02-27 17:52:53 +0100

    chore: format code

--- api-app/src/main/resources/db/migration/V1__activities.sql
+++ api-app/src/main/resources/db/migration/V1__activities.sql
@@ -1,10 +1,8 @@
 CREATE TABLE activities (
-  timestamp timestamp WITH TIME ZONE NOT NULL,
-  duration interval second(12) NOT NULL,
-  client varchar(200) NOT NULL,
-  project varchar(200) NOT NULL,
-  task varchar(200) NOT NULL,
-  notes varchar(500),
-
-  PRIMARY KEY (timestamp)
+  timestamp TIMESTAMP WITH TIME ZONE PRIMARY KEY,
+  duration  INTERVAL SECOND(12)      NOT NULL,
+  client    VARCHAR(200)             NOT NULL,
+  project   VARCHAR(200)             NOT NULL,
+  task      VARCHAR(200)             NOT NULL,
+  notes     VARCHAR(500)
 );
