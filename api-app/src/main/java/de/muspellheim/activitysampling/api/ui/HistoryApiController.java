7765d6b72a6a4af6af68a61e384f7fe118ae4043
Falko Schumann <falko.schumann@muspellheim.de>
2025-03-14 17:43:00 +0100

    fix: style issue

--- api-app/src/main/java/de/muspellheim/activitysampling/api/ui/HistoryApiController.java
+++ api-app/src/main/java/de/muspellheim/activitysampling/api/ui/HistoryApiController.java
@@ -1,9 +1,11 @@
+// Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
+
 package de.muspellheim.activitysampling.api.ui;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
+// @Controller
 public class HistoryApiController {
 
   @RequestMapping("/{path:[^.]*}")
