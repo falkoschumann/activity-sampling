6d3cf512c5987bb48219bba0a1585cddf80f7705
Falko Schumann <falko.schumann@muspellheim.de>
2025-06-08 08:40:04 +0200

    chore: remove database files

--- api-app/src/test/resources/db/migration/V9999__test-data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO activities (timestamp, duration, client, project, task, notes)
-VALUES
-  ('2024-12-18 09:30 Europe/Berlin', INTERVAL '30' MINUTE, 'ACME Inc.', 'Foobar', 'Do something', NULL),
-  ('2024-12-17 17:00 Europe/Berlin', INTERVAL '30' MINUTE, 'ACME Inc.', 'Foobar', 'Do something', NULL),
-  ('2024-12-17 16:30 Europe/Berlin', INTERVAL '30' MINUTE, 'ACME Inc.', 'Foobar', 'Do something', NULL),
-  ('2024-12-17 16:00 Europe/Berlin', INTERVAL '30' MINUTE, 'ACME Inc.', 'Foobar', 'Make things', 'This is a note');
