31685a4e89253f0ad08f325b0f87a8f943efc64d
Falko Schumann <falko.schumann@muspellheim.de>
2025-06-29 21:02:35 +0200

    refactor: move import to reader as main method

--- api-app/src/main/java/de/muspellheim/activitysampling/api/infrastructure/CalenderReader.java
+++ api-app/src/main/java/de/muspellheim/activitysampling/api/infrastructure/CalenderReader.java
@@ -2,10 +2,14 @@
 
 package de.muspellheim.activitysampling.api.infrastructure;
 
+import de.muspellheim.activitysampling.api.domain.activities.Holiday;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
@@ -13,6 +17,24 @@ import net.fortuna.ical4j.model.component.CalendarComponent;
 import net.fortuna.ical4j.model.component.VEvent;
 
 public class CalenderReader {
+
+  public static void main(String[] args) {
+    var eventsReader = new CalenderReader(Paths.get("data/sachsen.ics"));
+    var events = eventsReader.readEvents();
+
+    var holidays =
+        events.stream()
+            .map(it -> Holiday.builder().date((LocalDate) it.dtStart()).title(it.summary()).build())
+            .sorted(Comparator.comparing(Holiday::date))
+            .toList();
+    var holidayRepository =
+        new CsvHolidayRepository(
+            CsvHolidayRepositoryConfiguration.builder()
+                .file(Paths.get("data/holidays.csv"))
+                .build());
+    holidayRepository.save(holidays);
+  }
+
   private final Path file;
 
   public CalenderReader(Path file) {
--- api-app/src/test/java/de/muspellheim/activitysampling/api/integration/CalenderReaderTests.java
+++ api-app/src/test/java/de/muspellheim/activitysampling/api/integration/CalenderReaderTests.java
@@ -4,16 +4,11 @@ package de.muspellheim.activitysampling.api.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import de.muspellheim.activitysampling.api.domain.activities.Holiday;
 import de.muspellheim.activitysampling.api.infrastructure.CalenderReader;
-import de.muspellheim.activitysampling.api.infrastructure.CsvHolidayRepository;
-import de.muspellheim.activitysampling.api.infrastructure.CsvHolidayRepositoryConfiguration;
 import de.muspellheim.activitysampling.api.infrastructure.Event;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CalenderReaderTests {
@@ -29,24 +24,5 @@ class CalenderReaderTests {
     var ostermontag =
         Event.builder().dtStart(LocalDate.parse("2025-04-21")).summary("Ostermontag").build();
     assertEquals(List.of(karfreitag, ostermontag), events);
-  }
-
-  @Test
-  @Disabled
-  void importHolidays() {
-    var eventsReader = new CalenderReader(Paths.get("data/sachsen.ics"));
-    var events = eventsReader.readEvents();
-
-    var holidays =
-        events.stream()
-            .map(it -> Holiday.builder().date((LocalDate) it.dtStart()).title(it.summary()).build())
-            .sorted(Comparator.comparing(Holiday::date))
-            .toList();
-    var holidayRepository =
-        new CsvHolidayRepository(
-            CsvHolidayRepositoryConfiguration.builder()
-                .file(Paths.get("data/holidays.csv"))
-                .build());
-    holidayRepository.save(holidays);
   }
 }
