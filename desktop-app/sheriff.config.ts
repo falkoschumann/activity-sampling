39b911c1761ce1bec6c4c6045fc01623b5c5d4da
Falko Schumann <falko.schumann@muspellheim.de>
2025-08-11 08:34:36 +0200

    build: create common component

--- desktop-app/sheriff.config.ts
+++ desktop-app/sheriff.config.ts
@@ -4,6 +4,7 @@ import { SheriffConfig } from "@softarc/sheriff-core";
 
 export const config: SheriffConfig = {
   entryPoints: {
+    common: "src/common/index.ts",
     main: "src/main/index.ts",
     preload: "src/preload/index.ts",
     renderer: "src/renderer/index.tsx",
@@ -32,6 +33,12 @@ export const config: SheriffConfig = {
       "layer:infrastructure",
       "component:renderer",
     ],
+
+    "src/common": ["layer:entry", "component:common"],
+    "src/common/application": ["layer:application", "component:common"],
+    "src/common/common": ["layer:common", "component:common"],
+    "src/common/domain": ["layer:domain", "component:common"],
+    "src/common/infrastructure": ["layer:infrastructure", "component:common"],
   },
   depRules: {
     // root is a virtual module, which contains all files not being part
@@ -45,8 +52,8 @@ export const config: SheriffConfig = {
     "layer:application": ["layer:domain", "layer:infrastructure"],
     "layer:infrastructure": ["layer:domain"],
     "layer:*": ["layer:common"],
-    "component:main": ["layer:*"],
-    "component:preload": ["layer:*", "component:main"],
-    "component:renderer": ["layer:*", "component:main"],
+    "component:main": ["layer:*", "component:common"],
+    "component:preload": ["layer:*", "component:common"],
+    "component:renderer": ["layer:*", "component:common"],
   },
 };
