d764696e354c7bf60cc223600d4d747cefb8271f
Falko Schumann <falko.schumann@muspellheim.de>
2025-08-13 09:57:59 +0200

    feat: start implementing file based event store

--- desktop-app/src/main/infrastructure/event_store.ts
+++ desktop-app/src/main/infrastructure/event_store.ts
@@ -2,30 +2,57 @@
 
 // TODO store events in CSV file
 
+import * as csv from "csv";
+import fsPromise from "node:fs/promises";
+import path from "node:path";
+
 import { ConfigurableResponses } from "../../shared/common/configurable_responses";
 import { OutputTracker } from "../../shared/common/output_tracker";
 
 const RECORDED_EVENT = "recorded";
 
+export const TIMESTAMP_COLUMN: string = "Timestamp";
+export const DURATION_COLUMN: string = "Duration";
+export const CLIENT_COLUMN: string = "Client";
+export const PROJECT_COLUMN: string = "Project";
+export const TASK_COLUMN: string = "Task";
+export const NOTES_COLUMN: string = "Notes";
+
+const ACTIVITY_COLUMNS = [
+  TIMESTAMP_COLUMN,
+  DURATION_COLUMN,
+  CLIENT_COLUMN,
+  PROJECT_COLUMN,
+  TASK_COLUMN,
+  NOTES_COLUMN,
+];
+
 export class EventStore<T = unknown> extends EventTarget {
-  static create<T>(): EventStore<T> {
-    return new EventStore<T>();
+  static create<T>(fileName = "data/events.csv"): EventStore<T> {
+    return new EventStore<T>(fileName, fsPromise);
   }
 
   static createNull<T>({
     events,
   }: { events?: T[][] | T[] } = {}): EventStore<T> {
-    return new EventStore<T>(events);
+    return new EventStore<T>("null-file-csv", events);
   }
 
+  #fileName: string;
   #replayResponses: ConfigurableResponses<T[]>;
 
-  constructor(replayResponses?: T[][] | T[]) {
+  constructor(fileName: string, replayResponses?: T[][] | T[]) {
     super();
+    this.#fileName = fileName;
     this.#replayResponses = ConfigurableResponses.create<T[]>(replayResponses);
   }
 
   async record(event: T) {
+    const dirName = path.resolve(path.dirname(this.#fileName));
+    await fsPromise.mkdir(dirName, { recursive: true });
+
+    const header = false; // TODO set to true ife file does not exist
+    const stringifier = csv.stringify({ columns: ACTIVITY_COLUMNS, header });
     this.dispatchEvent(new CustomEvent(RECORDED_EVENT, { detail: event }));
   }
 
@@ -35,5 +62,11 @@ export class EventStore<T = unknown> extends EventTarget {
 
   async replay(): Promise<T[]> {
     return this.#replayResponses.next();
+  }
+}
+
+class FsPromiseStub {
+  async mkdir() {
+    return Promise.resolve(undefined);
   }
 }
--- desktop-app/test/integration/event_store.test.ts
+++ desktop-app/test/integration/event_store.test.ts
@@ -6,22 +6,33 @@ import { EventStore } from "../../src/main/infrastructure/event_store";
 import { createTestActivityLoggedEvent } from "../../src/main/infrastructure/events";
 
 describe("Event store", () => {
-  it("Records a event", async () => {
-    const store = EventStore.createNull();
-    const recordEvents = store.trackRecorded();
+  it("Records and replays events", async () => {
+    const store = EventStore.create();
 
     await store.record(createTestActivityLoggedEvent);
-
-    expect(recordEvents.data).toEqual([createTestActivityLoggedEvent]);
-  });
-
-  it("Replays events", async () => {
-    const store = EventStore.createNull({
-      events: [[createTestActivityLoggedEvent]],
-    });
-
     const events = await store.replay();
 
     expect(events).toEqual([createTestActivityLoggedEvent]);
+  });
+
+  describe("Nulled event store", () => {
+    it("Records a event", async () => {
+      const store = EventStore.createNull();
+      const recordEvents = store.trackRecorded();
+
+      await store.record(createTestActivityLoggedEvent);
+
+      expect(recordEvents.data).toEqual([createTestActivityLoggedEvent]);
+    });
+
+    it("Replays events", async () => {
+      const store = EventStore.createNull({
+        events: [[createTestActivityLoggedEvent]],
+      });
+
+      const events = await store.replay();
+
+      expect(events).toEqual([createTestActivityLoggedEvent]);
+    });
   });
 });
