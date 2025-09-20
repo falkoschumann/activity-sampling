624b4a7cc344f3cb91979493fa60c339586af348
Falko Schumann <falko.schumann@muspellheim.de>
2025-09-20 09:48:48 +0200

    fix: break component boundary

--- desktop-app/sheriff.config.ts
+++ desktop-app/sheriff.config.ts
@@ -1,16 +1,16 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
-import { SheriffConfig } from "@softarc/sheriff-core";
+import { sameTag, SheriffConfig } from "@softarc/sheriff-core";
 
 export const config: SheriffConfig = {
+  autoTagging: false,
+  enableBarrelLess: true,
+  barrelFileName: "mod.ts",
   entryPoints: {
     main: "src/main/index.ts",
     preload: "src/preload/index.ts",
     renderer: "src/renderer/index.tsx",
   },
-  barrelFileName: "mod.ts",
-  enableBarrelLess: true,
-  excludeRoot: true,
   modules: {
     "src/main": ["component:main", "layer:entry"],
     "src/main/application": ["component:main", "layer:application"],
@@ -31,17 +31,11 @@ export const config: SheriffConfig = {
     "src/shared/infrastructure": ["component:shared", "layer:infrastructure"],
   },
   depRules: {
-    // root is a virtual module, which contains all files not being part
-    // of any module, e.g. application shell, main.ts, etc.
-    root: "noTag",
-    noTag: "noTag",
-
-    // add your dependency rules here
-    "component:*": ["component:shared", "layer:*"],
+    "component:*": [sameTag, "component:shared"],
     "layer:entry": ["layer:*"],
     "layer:ui": ["layer:application", "layer:domain"],
     "layer:application": ["layer:domain", "layer:infrastructure"],
     "layer:infrastructure": ["layer:domain"],
-    "layer:*": ["layer:common"],
+    "layer:*": [sameTag, "layer:common"],
   },
 };
