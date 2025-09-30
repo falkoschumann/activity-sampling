28776df86331f4897243a0d83b89479865c8f94f
Falko Schumann <falko.schumann@muspellheim.de>
2025-09-30 21:31:21 +0200

    feat: initial period state

--- desktop-app/src/renderer/domain/period.ts
+++ desktop-app/src/renderer/domain/period.ts
@@ -1,5 +1,9 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
+import { Temporal } from "@js-temporal/polyfill";
+
+import type { FluxStandardAction } from "../common/reducer";
+
 export const PeriodUnit = Object.freeze({
   DAY: "Day",
   WEEK: "Week",
@@ -9,3 +13,60 @@ export const PeriodUnit = Object.freeze({
 });
 
 export type PeriodUnit = (typeof PeriodUnit)[keyof typeof PeriodUnit];
+
+//
+// Actions and Action Creators
+//
+
+export type Action = FluxStandardAction;
+
+//
+// State and Reducer
+//
+
+export interface State {
+  from: string;
+  to: string;
+  unit: PeriodUnit;
+  isCurrent: boolean;
+}
+
+export function init({
+  today,
+  unit,
+}: {
+  today: string;
+  unit: PeriodUnit;
+}): State {
+  const todayDate = Temporal.PlainDate.from(today);
+  if (unit === PeriodUnit.WEEK) {
+    const from = todayDate.subtract({ days: todayDate.dayOfWeek - 1 });
+    const to = todayDate.add({ days: 7 - todayDate.dayOfWeek });
+    return {
+      from: from.toString(),
+      to: to.toString(),
+      unit: unit,
+      isCurrent: true,
+    };
+  }
+
+  if (unit === PeriodUnit.MONTH) {
+    const from = todayDate.with({ day: 1 });
+    const to = todayDate.with({ day: 31 });
+    return {
+      from: from.toString(),
+      to: to.toString(),
+      unit: unit,
+      isCurrent: true,
+    };
+  }
+
+  throw new Error(`Unknown period in period initializer: ${unit}.`);
+}
+
+export function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- desktop-app/test/renderer/unit/period.test.ts
+++ desktop-app/test/renderer/unit/period.test.ts
@@ -1,12 +1,75 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import { init, PeriodUnit } from "../../../src/renderer/domain/period";
 
 describe("Period", () => {
   describe("Initialize period", () => {
-    it.todo("should initialize with current week");
+    it("should initialize with current week on Wednesday", () => {
+      const state = init({ today: "2025-08-13", unit: PeriodUnit.WEEK });
 
-    it.todo("should initialize with current month");
+      expect(state).toEqual({
+        from: "2025-08-11",
+        to: "2025-08-17",
+        unit: PeriodUnit.WEEK,
+        isCurrent: true,
+      });
+    });
+
+    it("should initialize with current week on Monday", () => {
+      const state = init({ today: "2025-09-29", unit: PeriodUnit.WEEK });
+
+      expect(state).toEqual({
+        from: "2025-09-29",
+        to: "2025-10-05",
+        unit: PeriodUnit.WEEK,
+        isCurrent: true,
+      });
+    });
+
+    it("should initialize with current week on Sunday", () => {
+      const state = init({ today: "2025-09-28", unit: PeriodUnit.WEEK });
+
+      expect(state).toEqual({
+        from: "2025-09-22",
+        to: "2025-09-28",
+        unit: PeriodUnit.WEEK,
+        isCurrent: true,
+      });
+    });
+
+    it("should initialize current month with 30 days", () => {
+      const state = init({ today: "2025-09-20", unit: PeriodUnit.MONTH });
+
+      expect(state).toEqual({
+        from: "2025-09-01",
+        to: "2025-09-30",
+        unit: PeriodUnit.MONTH,
+        isCurrent: true,
+      });
+    });
+
+    it("should initialize current month with 31 days", () => {
+      const state = init({ today: "2025-08-20", unit: PeriodUnit.MONTH });
+
+      expect(state).toEqual({
+        from: "2025-08-01",
+        to: "2025-08-31",
+        unit: PeriodUnit.MONTH,
+        isCurrent: true,
+      });
+    });
+
+    it("should initialize current month with 28 days", () => {
+      const state = init({ today: "2025-02-20", unit: PeriodUnit.MONTH });
+
+      expect(state).toEqual({
+        from: "2025-02-01",
+        to: "2025-02-28",
+        unit: PeriodUnit.MONTH,
+        isCurrent: true,
+      });
+    });
   });
 
   describe("Change period", () => {
