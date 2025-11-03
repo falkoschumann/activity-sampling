b71d1b9eb90c4a9fd632058a6577dc17de72c9a0
Falko Schumann <falko.schumann@muspellheim.de>
2025-11-03 18:52:51 +0100

    fix: import vitest configuration

--- desktop-app/vitest.config.ts
+++ desktop-app/vitest.config.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
-/// <reference types="vitest" />
-
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
