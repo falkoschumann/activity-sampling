3ab024cb9263e6a9cfe2ff795815f27b943c3da0
Falko Schumann <falko.schumann@muspellheim.de>
2025-12-14 21:03:02 +0100

    refactor: do not filter events twice

--- desktop-app/src/main/domain/report_projection.ts
+++ desktop-app/src/main/domain/report_projection.ts
@@ -33,11 +33,9 @@ export async function projectReport(
     default:
       throw new Error(`Unknown scope: ${query.scope}`);
   }
-  // TODO replace activity projection with event projection
-  const activities = await projectActivities(replay, query.from, query.to);
-  for await (const activity of activities) {
-    projection.update(activity);
-    totalHoursProjection.update(activity);
+  for await (const event of filterEvents(replay, query.from, query.to)) {
+    projection.update(event);
+    totalHoursProjection.update(event);
   }
   return {
     entries: projection.get(),
@@ -65,121 +63,26 @@ async function* filterEvents(
   }
 }
 
-class Activity {
-  static create({
-    start,
-    finish,
-    client,
-    project,
-    task,
-    category,
-    hours,
-  }: {
-    start: Temporal.PlainDateLike | string;
-    finish: Temporal.PlainDateLike | string;
-    client: string;
-    project: string;
-    task: string;
-    category?: string;
-    hours: Temporal.DurationLike | string;
-  }): Activity {
-    return new Activity(start, finish, client, project, task, hours, category);
-  }
-
-  readonly start: Temporal.PlainDate;
-  readonly finish: Temporal.PlainDate;
-  readonly client: string;
-  readonly project: string;
-  readonly task: string;
-  readonly category?: string;
-  readonly hours: Temporal.Duration;
-
-  private constructor(
-    start: Temporal.PlainDateLike | string,
-    finish: Temporal.PlainDateLike | string,
-    client: string,
-    project: string,
-    task: string,
-    hours: Temporal.DurationLike | string,
-    category?: string,
-  ) {
-    this.start = Temporal.PlainDate.from(start);
-    this.finish = Temporal.PlainDate.from(finish);
-    this.client = client;
-    this.project = project;
-    this.task = task;
-    this.category = category;
-    this.hours = Temporal.Duration.from(hours);
-  }
-}
-
-export async function projectActivities(
-  replay: AsyncGenerator<ActivityLoggedEvent>,
-  from?: Temporal.PlainDateLike | string,
-  to?: Temporal.PlainDateLike | string,
-): Promise<Activity[]> {
-  const activities: Activity[] = [];
-  for await (const event of filterEvents(replay, from, to)) {
-    const date = event.dateTime.toPlainDate();
-    const index = activities.findIndex(
-      (activity) =>
-        activity.client === event.client &&
-        activity.project === event.project &&
-        activity.task === event.task &&
-        activity.category === event.category,
-    );
-    if (index === -1) {
-      const activity = Activity.create({
-        start: date,
-        finish: date,
-        client: event.client,
-        project: event.project,
-        task: event.task,
-        category: event.category,
-        hours: event.duration,
-      });
-      activities.push(activity);
-    } else {
-      const activity = activities[index];
-      let start = activity.start;
-      let finish = activity.finish;
-      if (Temporal.PlainDate.compare(date, start) < 0) {
-        start = date;
-      }
-      if (Temporal.PlainDate.compare(date, finish) > 0) {
-        finish = date;
-      }
-      const accumulatedHours = activity.hours.add(event.duration);
-      activities[index] = {
-        ...activity,
-        start,
-        finish,
-        hours: normalizeDuration(accumulatedHours),
-      };
-    }
-  }
-  return activities;
-}
-
 class ClientReportProjection {
   #entries: ReportEntry[] = [];
 
-  update(activity: Activity) {
+  update(event: ActivityLoggedEvent) {
     const index = this.#entries.findIndex(
-      (entry) => entry.client === activity.client,
+      (entry) => entry.client === event.client,
     );
     if (index == -1) {
+      const date = event.dateTime.toPlainDate();
       this.#entries.push(
         ReportEntry.create({
-          start: activity.start,
-          finish: activity.finish,
-          client: activity.client,
-          hours: activity.hours,
-          cycleTime: activity.finish.since(activity.start).total("days") + 1,
+          start: date,
+          finish: date,
+          client: event.client,
+          hours: event.duration,
+          cycleTime: 1,
         }),
       );
     } else {
-      this.#entries[index] = updateEntry(this.#entries[index], activity);
+      this.#entries[index] = updateEntry(this.#entries[index], event);
     }
   }
 
@@ -191,27 +94,24 @@ class ClientReportProjection {
 class ProjectReportProjection {
   #entries: ReportEntry[] = [];
 
-  update(activity: Activity) {
+  update(event: ActivityLoggedEvent) {
     const index = this.#entries.findIndex(
-      (entry) => entry.project === activity.project,
+      (entry) => entry.project === event.project,
     );
     if (index == -1) {
+      const date = event.dateTime.toPlainDate();
       this.#entries.push(
         ReportEntry.create({
-          start: activity.start,
-          finish: activity.finish,
-          client: activity.client,
-          project: activity.project,
-          hours: activity.hours,
-          cycleTime: activity.finish.since(activity.start).total("days") + 1,
+          start: date,
+          finish: date,
+          client: event.client,
+          project: event.project,
+          hours: event.duration,
+          cycleTime: 1,
         }),
       );
     } else {
-      this.#entries[index] = updateEntry(
-        this.#entries[index],
-        activity,
-        "client",
-      );
+      this.#entries[index] = updateEntry(this.#entries[index], event, "client");
     }
   }
 
@@ -230,30 +130,31 @@ class ProjectReportProjection {
 class TaskReportProjection {
   #entries: ReportEntry[] = [];
 
-  update(activity: Activity) {
+  update(event: ActivityLoggedEvent) {
     const index = this.#entries.findIndex(
       (entry) =>
-        entry.task === activity.task &&
-        entry.project === activity.project &&
-        entry.client === activity.client,
+        entry.task === event.task &&
+        entry.project === event.project &&
+        entry.client === event.client,
     );
     if (index == -1) {
+      const date = event.dateTime.toPlainDate();
       this.#entries.push(
         ReportEntry.create({
-          start: activity.start,
-          finish: activity.finish,
-          client: activity.client,
-          project: activity.project,
-          task: activity.task,
-          category: activity.category,
-          hours: activity.hours,
-          cycleTime: activity.finish.since(activity.start).total("days") + 1,
+          start: date,
+          finish: date,
+          client: event.client,
+          project: event.project,
+          task: event.task,
+          category: event.category,
+          hours: event.duration,
+          cycleTime: 1,
         }),
       );
     } else {
       this.#entries[index] = updateEntry(
         this.#entries[index],
-        activity,
+        event,
         "category",
       );
     }
@@ -284,22 +185,23 @@ class TaskReportProjection {
 class CategoryReportProjection {
   #entries: ReportEntry[] = [];
 
-  update(activity: Activity) {
+  update(event: ActivityLoggedEvent) {
     const index = this.#entries.findIndex(
-      (entry) => entry.category === (activity.category ?? ""),
+      (entry) => entry.category === (event.category ?? ""),
     );
     if (index == -1) {
+      const date = event.dateTime.toPlainDate();
       this.#entries.push(
         ReportEntry.create({
-          start: activity.start,
-          finish: activity.finish,
-          category: activity.category,
-          hours: activity.hours,
-          cycleTime: activity.finish.since(activity.start).total("days") + 1,
+          start: date,
+          finish: date,
+          category: event.category,
+          hours: event.duration,
+          cycleTime: 1,
         }),
       );
     } else {
-      this.#entries[index] = updateEntry(this.#entries[index], activity);
+      this.#entries[index] = updateEntry(this.#entries[index], event);
     }
   }
 
@@ -310,19 +212,16 @@ class CategoryReportProjection {
 
 function updateEntry(
   entry: ReportEntry,
-  activity: Activity,
+  event: ActivityLoggedEvent,
   groupBy?: "client" | "category",
 ): ReportEntry {
+  const date = event.dateTime.toPlainDate();
   const start =
-    Temporal.PlainDate.compare(activity.start, entry.start) < 0
-      ? activity.start
-      : entry.start;
+    Temporal.PlainDate.compare(date, entry.start) < 0 ? date : entry.start;
   const finish =
-    Temporal.PlainDate.compare(activity.finish, entry.finish) < 0
-      ? entry.finish
-      : activity.finish;
+    Temporal.PlainDate.compare(date, entry.finish) < 0 ? entry.finish : date;
   const cycleTime = finish.since(start).total("days") + 1;
-  const accumulatedHours = entry.hours.add(activity.hours);
+  const accumulatedHours = entry.hours.add(event.duration);
   const updatedEntry = {
     ...entry,
     start,
@@ -332,11 +231,11 @@ function updateEntry(
   };
   if (
     groupBy != null &&
-    activity[groupBy] != null &&
-    !updatedEntry[groupBy].includes(activity[groupBy])
+    event[groupBy] != null &&
+    !updatedEntry[groupBy].includes(event[groupBy])
   ) {
     let groups = updatedEntry[groupBy].split(", ");
-    groups.push(activity[groupBy]);
+    groups.push(event[groupBy]);
     groups = groups.sort();
     updatedEntry[groupBy] = groups.join(", ");
   }
@@ -346,8 +245,8 @@ function updateEntry(
 class TotalHoursProjection {
   #totalHours = Temporal.Duration.from("PT0S");
 
-  update(activity: Activity) {
-    this.#totalHours = this.#totalHours.add(activity.hours);
+  update(event: ActivityLoggedEvent) {
+    this.#totalHours = this.#totalHours.add(event.duration);
   }
 
   get() {
