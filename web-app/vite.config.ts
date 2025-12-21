02e7dec73db3901afe1d78695866e6f3de32a02d
Falko Schumann <falko.schumann@muspellheim.de>
2025-12-21 19:33:52 +0100

    chore: unify configuration

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -21,7 +21,12 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ["import", "color-functions", "global-builtin"],
+        silenceDeprecations: [
+          "import",
+          "color-functions",
+          "global-builtin",
+          "if-function",
+        ],
       },
     },
   },
